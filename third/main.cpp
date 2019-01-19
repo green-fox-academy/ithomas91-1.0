64e8b42d760a7763b0f8bc4a25fdcffa712d5a40
Tamas Nemeth <ithomas.nemeth@gmail.com>
2019-01-19 18:42:52 +0100

    +.idea/

---
+++ third/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+// - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+
+
+int main(int argc, char* args[]) {
+
+    int q[] {4, 5, 6, 7};
+
+    return 0;
+}
