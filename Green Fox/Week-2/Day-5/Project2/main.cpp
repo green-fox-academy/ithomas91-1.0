d7ae5dc35729c4de984601719e84372d5d064f1d
Tamas Nemeth <ithomas.nemeth@gmail.com>
2019-01-25 14:28:12 +0100

    project2

--- Green Fox/Week-2/Day-5/Project2/main.cpp
+++ Green Fox/Week-2/Day-5/Project2/main.cpp
@@ -22,56 +22,43 @@ SDL_Renderer* gRenderer = nullptr;
 
 void draw()
 {
-    int x1 = SCREEN_WIDTH;
-    int y2 = SCREEN_HEIGHT;
+
+    int x1 = 320;
+    int y2 = 0;
 
 
+    for(int i = 0; i < 16; i++) {
 
-    for(int i = 0; i < 32; i++){
 
 
         SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-        SDL_RenderDrawLine(gRenderer,  x1 , SCREEN_HEIGHT, 0, y2);
+        SDL_RenderDrawLine(gRenderer, x1, 320, 320, y2);
 
+        x1 -= 20;
+        y2 += 20;
 
-        x1 -=20;
-        y2 -=20;
     }
-
 
     int x = SCREEN_WIDTH;
     int y = SCREEN_HEIGHT;
 
 
 
-    for(int i = 0; i < 32; i++){
+    for(int i = 0; i < 16; i++) {
 
 
-        SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
-        SDL_RenderDrawLine(gRenderer, x, 0, 640, y);
+
+        SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+        SDL_RenderDrawLine(gRenderer, 0, 320, 320, 640);
+
+        SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+        SDL_RenderDrawLine(gRenderer, 0, 320, 320, 620);
 
 
-        x -=20;
-        y -=20;
     }
 
-    SDL_Delay(500);
-
-    /* SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-    SDL_RenderDrawLine(gRenderer,  620, 480, 0, 440);
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-    SDL_RenderDrawLine(gRenderer,  600, 480, 0, 420);
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-    SDL_RenderDrawLine(gRenderer,  580, 480, 0, 400);
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-    SDL_RenderDrawLine(gRenderer,  560, 480, 0, 380);
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-    SDL_RenderDrawLine(gRenderer,  540, 480, 0, 360);
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-    SDL_RenderDrawLine(gRenderer,  520, 480, 0, 340);
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-    SDL_RenderDrawLine(gRenderer,  500, 480, 0, 320);*/
-
+    // Draw the canvas' diagonals.
+    // If it starts from the upper-left corner it should be green, otherwise it should be red.
 
 }
 
@@ -85,7 +72,7 @@ bool init()
     }
 
     //Create window
-    gWindow = SDL_CreateWindow( "Starry night", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+    gWindow = SDL_CreateWindow( "Diagonals", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE );
     if( gWindow == nullptr )
     {
         std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
@@ -117,11 +104,9 @@ void close()
     SDL_Quit();
 }
 
-int main( int argc, char* args[] )
-{
+int main( int argc, char* args[] ) {
     //Start up SDL and create window
-    if( !init() )
-    {
+    if (!init()) {
         std::cout << "Failed to initialize!" << std::endl;
         close();
         return -1;
@@ -134,7 +119,7 @@ int main( int argc, char* args[] )
     SDL_Event e;
 
     //While application is running
-    while( !quit ) {
+    while (!quit) {
         //Handle events on queue
         while (SDL_PollEvent(&e) != 0) {
             //User requests quit
