fb8df9f745c6f85032f026c60abeaeb5a7189a42
Tamas Nemeth <ithomas.nemeth@gmail.com>
2019-04-23 11:49:34 +0200

    refactor print elements, and five numbers

--- foundation/Week-1/Day-4/print elements/main.cpp
+++ foundation/Week-1/Day-4/print elements/main.cpp
@@ -14,10 +14,26 @@
 //
 // - Print this two dimensional array to the output
 
-
+void createMatrix(int userInput){
+    for (int i = 0; i < userInput; ++i) {
+        for (int j = 0; j < userInput; ++j) {
+            if(i == j){
+                std::cout<< "1 ";
+            } else {
+                std::cout << "0 ";
+            }
+        }
+        std::cout<<std::endl;
+    }
+}
 
 int main(int argc, char* args[]) {
 
+    int userInput;
+    std::cout<<"Prease enter an integer to get the size of the matrix what i will make for you!"<<std::endl;
+    std::cin>> userInput;
+
+    createMatrix(userInput);
 
     return 0;
 }
--- foundation/Week-2/Day-2/five numbers/main.cpp
+++ foundation/Week-2/Day-2/five numbers/main.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
+#include <vector>
+
 
 // Create a program which accepts five integers from the console (given by the user)
     // and store them in an array
     // print out the values of that array using pointers again
 
+void printValue(int a, int b, int c, int d, int e) {
+
+    std::vector<int> vector;
+    vector.push_back(a);
+    vector.push_back(b);
+    vector.push_back(c);
+    vector.push_back(d);
+    vector.push_back(e);
+
+    for (int i = 0; i < vector.size(); ++i) {
+        std::cout << vector[i] << " "<<std::endl;
+    }
+}
 
 int main()
 {
 
-    std::cout<<"Enter 5 integer!"<<std::endl;
+    int a, b, c, d, e;
 
-    int array[5];
-    
-    for(int i = 0; i < sizeof(array)/ sizeof(array[0]); i++) {  // can be i < 5 , as it written
-
-        std::cin>> array[i];
-    }
-    /* int a;
-    int b;
-    int c;
-    int d;
-    int e;
-
+    std::cout<< "Please enter 5 integers next to each other, separated by space!" <<std::endl;
     std::cin >> a;
     std::cin >> b;
     std::cin >> c;
     std::cin >> d;
     std::cin >> e;
 
-    int array[] = {a, b, c, d, e};
-    */
 
-    int *arrayp = array;
+    printValue(a,b,c,d,e);
 
-    for(int i = 0; i < sizeof(array)/sizeof(array[i]); i++){
-        std::cout<< arrayp[i]<<std::endl;
-
-    }
     return 0;
 }
