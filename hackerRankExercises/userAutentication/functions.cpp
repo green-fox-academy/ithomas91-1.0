832c7a12375befbc602b459719f9aeab91b2f30d
Tamas Nemeth <ithomas.nemeth@gmail.com>
2019-05-20 16:00:51 +0200

    add database to own project

---
+++ hackerRankExercises/userAutentication/functions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tamas Nemeth on 2019. 05. 16..
+//
+
+#include "functions.h"
---
+++ hackerRankExercises/userAutentication/functions.h
@@ -0,0 +1,14 @@
+//
+// Created by Tamas Nemeth on 2019. 05. 16..
+//
+
+#ifndef USERAUTENTICATION_FUNCTIONS_H
+#define USERAUTENTICATION_FUNCTIONS_H
+
+
+class functions {
+
+};
+
+
+#endif //USERAUTENTICATION_FUNCTIONS_H
