0d6d93beb0653198cfb7edd3b9c2352d6dde9db2
Tamas Nemeth <ithomas.nemeth@gmail.com>
2019-06-19 11:54:08 +0200

    first javaScript project

--- Else/javaScript/FirstJSproject/indexJs.js
+++ Else/javaScript/FirstJSproject/indexJs.js
@@ -1,23 +1,9 @@
-let a = 10;
-let b = 20;
 
-console.log(a, b);
 
-function swapFunction (patameter1, parameter2) {
-    let temp = a;
-    a = b;
-    b = temp;
+function clickToRegister(){
+    alert("You have clicked to Register");
 }
 
-swapFunction(a, b);
-console.log (a, b);
-
-// print all even numberst to 100
-
-
-for (let i = 0; i < 100; i++){
-    if (i % 2 == 1){
-        console.log(i);
-    }
+function clickToLogin(){
+    alert("You have clicked to Login");
 }
-
