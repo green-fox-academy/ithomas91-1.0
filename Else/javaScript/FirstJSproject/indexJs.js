18af12f1b109778e24eab99b7e2a565163a6cd46
Tamas Nemeth <ithomas.nemeth@gmail.com>
2019-07-08 13:17:26 +0200

    home project js

--- Else/javaScript/FirstJSproject/indexJs.js
+++ Else/javaScript/FirstJSproject/indexJs.js
@@ -25,3 +25,27 @@ function analyseClick(id){
     }   
 
 }
+
+let userData = {};
+let users = [];
+
+
+function getUserData() {
+    userData["Full Name"] = document.getElementById("name").value;
+    //userData.push(userFullName);
+    userData["username"] =  document.getElementById("username").value;
+    //userData.push(username);
+    userData["emailAdress"] =  document.getElementById("e-mail").value;
+    //userData.push(emailAdress);
+    userData["password"] =  document.getElementById("password").value;
+    //userData.push(password);
+    userData["confirmPassword"] =  document.getElementById("confirmpassword").value;
+    //userData.push(confirmPassword);
+    if(userData["Full Name"] != null || userData["username"] != null || 
+    userData["e-mail"] != null || userData["password"] != null
+    || userData["confirmPassword"] != null) {
+        users.push(userData);
+    } else {
+        alert("Empty field!");
+    }
+}
